a4d92461655975dd3f9d61fd1261b260b090214d
weblue <nader.kirrish@gmail.com>
2022-11-14 16:33:49 -0500

    forgot to add new files

---
+++ core.js
@@ -0,0 +1,37 @@
+function init() {
+    var themeSetting = localStorage.getItem('theme');
+    var theme = document.getElementById("style");
+    if (themeSetting == 'dark' || window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
+        theme.setAttribute('href', 'css/dark-styles.css');
+    else if (themeSetting == 'light')
+        theme.setAttribute('href', 'css/light-styles.css');
+
+    refresh();
+}
+
+function toggleTheme() {
+    var theme = document.getElementById("style");
+
+    if (theme.getAttribute('href') == 'css/light-styles.css') {
+        theme.setAttribute('href', 'css/dark-styles.css');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        theme.setAttribute('href', 'css/light-styles.css');
+        localStorage.setItem('theme', 'light');
+    }
+    refresh();
+}
+
+function refresh() {
+    var pixelBox = document.getElementById('pixel-box')
+    var n = document.createTextNode(' ');
+    var display = pixelBox.style.display;
+
+    pixelBox.appendChild(n);
+    pixelBox.style.display = 'none';
+
+    setTimeout(function () {
+        pixelBox.style.display = display;
+        n.parentNode.removeChild(n);
+    }, 20);
+}
