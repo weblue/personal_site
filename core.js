7a7afce7a8011c5dc6751b2057bbc6be29ab30af
weblue <nader.kirrish@gmail.com>
2022-11-17 11:53:55 -0500

    update content and styles

--- core.js
+++ core.js
@@ -25,6 +25,13 @@ function toggleTheme() {
 function showModal(project) {
     document.getElementById("open-modal").style.visibility = "visible";
     document.getElementById("open-modal").style.pointerEvents = "auto";
+
+    document.getElementById("project-name").innerHTML = projects[project].title;
+    document.getElementById("description").innerHTML = projects[project].description;
+
+    // TODO update these lists to contain links to relevant content
+    document.getElementById("skills")
+    document.getElementById("keywords")
 }
 
 function closeModal() {
@@ -44,3 +51,59 @@ function refresh() {
         n.parentNode.removeChild(n);
     }, 20);
 }
+
+
+//TODO update content
+var projects = {
+        "hertz": {
+            "title": "Hertz - Uber - Lyft",
+            "description": "this is where I talk to the bean",
+            "responsibilities": [
+                {
+                    "skill": "SRE",
+                    "url": "blog link",
+                    "description": "describe responsibilities"
+                },
+                {
+                    "skill": "Monitoring",
+                    "url": "",
+                    "description": "describe responsibilities"
+                },
+                {
+                    "skill": "Tech lead",
+                    "url": "blog link",
+                    "description": "describe responsibilities"
+                }
+            ],
+            "keywords": ["Kotlin", "Dynatrace", "Cloudwatch", "SLOs"]
+        },
+        "caimr": {
+            "description": "this is where I'll describe projects for caimr",
+            "responsibilities": [
+                {
+                    "skill": "Spring",
+                    "url": "",
+                    "description": "describe responsibilities"
+                },
+                {
+                    "skill": "AI",
+                    "url": "blog link",
+                    "description": "describe responsibilities"
+                },
+                {
+                    "skill": "Tech lead",
+                    "url": "blog link?",
+                    "description": "describe responsibilities"
+                }
+            ],
+            "keywords": ["AI + Health", "AWS", "ECS", "Big data"]
+        },
+        "eft": {
+            "description": "describe eft wannab",
+            "keywords": ["Vue.js"]
+        },
+        "cephalon": {
+            "description": "describe cephalon wannab",
+            "keywords": ["Angular"]
+        }
+    }
