b2e23eff6a6565d38bc639eb3e47209dd079ef91
weblue <nader.kirrish@gmail.com>
2022-11-17 21:23:47 -0500

    update modal for desktop and mobile formatting

--- core.js
+++ core.js
@@ -23,15 +23,73 @@ function toggleTheme() {
 }
 
 function showModal(project) {
-    document.getElementById("open-modal").style.visibility = "visible";
-    document.getElementById("open-modal").style.pointerEvents = "auto";
+    var modal = document.getElementById("open-modal")
+    modal.style.visibility = "visible";
+    modal.style.pointerEvents = "auto";
 
     document.getElementById("project-name").innerHTML = projects[project].title;
     document.getElementById("description").innerHTML = projects[project].description;
 
-    // TODO update these lists to contain links to relevant content
-    document.getElementById("skills")
-    document.getElementById("keywords")
+    //Get list and title
+    var ulSkills = document.getElementById("skills")
+    var h4Skills = document.getElementById("skills-title")
+    //Clear list
+    ulSkills.innerHTML = ""
+
+    //Hide elements if their data is empty
+    var resps = projects[project].responsibilities;
+    if (resps == undefined) {
+        console.error("missing property 'responsibilities'")
+    }
+    if (resps.length == 0) {
+        ulSkills.style.visibility = "hidden"
+        h4Skills.style.visibility = "hidden"
+    } else {
+        ulSkills.style.visibility = ""
+        h4Skills.style.visibility = ""
+    }
+    //Populate list elements, including urls, if they exist
+    resps.forEach(skill => {
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        a.textContent = skill.skill
+
+        var url = skill.url
+        if (url) {
+            a.setAttribute('href', url);
+        }
+
+        li.appendChild(a);
+        ulSkills.appendChild(li);
+    });
+
+    //Get list and title
+    var ulKeywords = document.getElementById("keywords")
+    var h4Keywords = document.getElementById("keywords-title")
+
+    //Clear list
+    ulKeywords.innerHTML = ""
+
+    //Hide elements if their data is empty
+    var keywords = projects[project].keywords;
+    if (keywords == undefined) {
+        console.error("missing property 'keywords'")
+    }
+    if (keywords.length == 0) {
+        ulKeywords.style.visibility = "hidden"
+        h4Keywords.style.visibility = "hidden"
+    } else {
+        ulKeywords.style.visibility = ""
+        h4Keywords.style.visibility = ""
+    }
+    keywords.forEach(keyword => {
+        var li = document.createElement("li");
+        var a = document.createElement("a");
+        a.textContent = keyword
+
+        li.appendChild(a);
+        ulKeywords.appendChild(li);
+    });
 }
 
 function closeModal() {
@@ -53,57 +111,56 @@ function refresh() {
 }
 
 
-//TODO update content
+//TODO LATER update content
 var projects = {
-        "hertz": {
-            "title": "Hertz - Uber - Lyft",
-            "description": "this is where I talk to the bean",
-            "responsibilities": [
-                {
-                    "skill": "SRE",
-                    "url": "blog link",
-                    "description": "describe responsibilities"
-                },
-                {
-                    "skill": "Monitoring",
-                    "url": "",
-                    "description": "describe responsibilities"
-                },
-                {
-                    "skill": "Tech lead",
-                    "url": "blog link",
-                    "description": "describe responsibilities"
-                }
-            ],
-            "keywords": ["Kotlin", "Dynatrace", "Cloudwatch", "SLOs"]
-        },
-        "caimr": {
-            "description": "this is where I'll describe projects for caimr",
-            "responsibilities": [
-                {
-                    "skill": "Spring",
-                    "url": "",
-                    "description": "describe responsibilities"
-                },
-                {
-                    "skill": "AI",
-                    "url": "blog link",
-                    "description": "describe responsibilities"
-                },
-                {
-                    "skill": "Tech lead",
-                    "url": "blog link?",
-                    "description": "describe responsibilities"
-                }
-            ],
-            "keywords": ["AI + Health", "AWS", "ECS", "Big data"]
-        },
-        "eft": {
-            "description": "describe eft wannab",
-            "keywords": ["Vue.js"]
-        },
-        "cephalon": {
-            "description": "describe cephalon wannab",
-            "keywords": ["Angular"]
-        }
+    "hertz": {
+        "title": "Hertz - Uber - Lyft",
+        "description": "this is where I talk to the bean",
+        "responsibilities": [
+            {
+                "skill": "SRE",
+                "url": "blog link",
+                "description": "describe responsibilities"
+            },
+            {
+                "skill": "Monitoring",
+                "url": "",
+                "description": "describe responsibilities"
+            },
+            {
+                "skill": "Tech lead",
+                "url": "blog link",
+                "description": "describe responsibilities"
+            }
+        ],
+        "keywords": ["Kotlin", "Dynatrace", "Cloudwatch", "SLOs"]
+    },
+    "caimr": {
+        "title": "CA Division of Workers' Compensation",
+        "description": "this is where I'll describe projects for caimr",
+        "responsibilities": [
+            {
+                "skill": "Spring",
+                "url": "",
+                "description": "describe responsibilities"
+            },
+            {
+                "skill": "AI",
+                "url": "blog link",
+                "description": "describe responsibilities"
+            },
+            {
+                "skill": "Tech lead",
+                "url": "blog link?",
+                "description": "describe responsibilities"
+            }
+        ],
+        "keywords": ["AI + Health", "AWS", "ECS", "Big data"]
+    },
+    "cephalon": {
+        "title": "Cephalon Wanna_b",
+        "description": "describe cephalon wannab",
+        "responsibilities": [],
+        "keywords": ["Angular"]
     }
+}
